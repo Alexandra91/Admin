78e8d701df6a1d8eba840f54d69f546e67b4ac5c
ninaramadan <ninaramadan@172.22.100.67>
2016-07-21 10:38:03 +0300

    commit adminProductPage and Steps, also AbstractPage and Steps

--- AdminToolbar/src/test/java/com/pages/Product/AdminProductPage.java
+++ AdminToolbar/src/test/java/com/pages/Product/AdminProductPage.java
@@ -1,5 +1,20 @@
 package com.pages.Product;
 
-public class AdminProductPage {
+import org.openqa.selenium.WebElement;
 
+import com.tools.AbstractPage;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+
+public class AdminProductPage extends AbstractPage {
+
+	@FindBy(css=".desktop-template a[data-fragment='admin']")
+	private WebElement adminTab;
+	
+	public void clickAdminTab(){
+		element(adminTab).waitUntilVisible();
+		adminTab.click();
+	}
+	
+	
 }
