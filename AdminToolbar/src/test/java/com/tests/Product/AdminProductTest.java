9c94ba7e61d9f30e210d8d51671f0526d057f4ff
Nina Ramadan <nina.ramadan@evozon.com>
2016-07-21 14:01:49 +0300

    updated project commit and gitignore,

--- AdminToolbar/src/test/java/com/tests/Product/AdminProductTest.java
+++ AdminToolbar/src/test/java/com/tests/Product/AdminProductTest.java
@@ -16,36 +16,37 @@ import net.thucydides.junit.annotations.UseTestDataFrom;
 
 //@RunWith(SerenityRunner.class)
 @RunWith(SerenityParameterizedRunner.class)
-@UseTestDataFrom(value="src/test/resources/tests/productLinks.csv")
-//@UseTestDataFrom(value="src/test/resources/tests/magentoAdmin.csv")
+@UseTestDataFrom(value = "src/test/resources/tests/productLinks.csv")
+// @UseTestDataFrom(value="src/test/resources/tests/magentoAdmin.csv")
 
 public class AdminProductTest {
-	 
-@Managed(uniqueSession = true)
-public WebDriver webdriver;
 
-@Steps
-public LogInSteps logInSteps;
-@Steps
-public AdminProductSteps adminProductSteps;
+	@Managed(uniqueSession = true)
+	public WebDriver webdriver;
 
+	@Steps
+	public LogInSteps logInSteps;
+	@Steps
+	public AdminProductSteps adminProductSteps;
 
-private String  productLink, baseURL,user,pass;
- 
-@Before
-public void dataSetup(){
-	baseURL="http://admintoolbar-qa-ce191.evozon.com/admin";
-	user="admin";
-	pass="admin123";
-//	productLink="http://admintoolbar-qa-ce191.evozon.com/plaid-cotton-shirt.html";
-	
-}
+	private String productLink, baseURL, user, pass;
 
-@Test
-public void tc001AdminProductTest(){
-    logInSteps.login_magentoAdmin(user, pass,baseURL);
-	adminProductSteps.navigateTo(productLink);
-	adminProductSteps.clickAdminTab();
-	
-}
+	@Before
+	public void dataSetup() {
+		baseURL = "http://admintoolbar-qa-ce191.evozon.com/admin";
+		user = "admin";
+		pass = "admin123";
+		// productLink="http://admintoolbar-qa-ce191.evozon.com/plaid-cotton-shirt.html";
+
+	}
+
+	@Test
+	public void tc001AdminProductTest() {
+		
+		System.out.println("baseURL: " + baseURL);
+		logInSteps.login_magentoAdmin(baseURL, user, pass);
+		adminProductSteps.navigateTo(productLink);
+		adminProductSteps.clickAdminTab();
+
+	}
 }
