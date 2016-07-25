9467dd5affb34ade7efcbe7912f7e7b2edb00bbe
Alexandra Pandele <alexandra.pandele@evozon.com>
2016-07-25 09:23:28 +0300

    Abstract Steps

--- src/test/java/com/tools/AbstractSteps.java
+++ src/test/java/com/tools/AbstractSteps.java
@@ -5,6 +5,7 @@ import net.thucydides.core.steps.ScenarioSteps;
 
 public class AbstractSteps extends ScenarioSteps{
 	
+	private static final long serialVersionUID = 1L;
 	private AbstractPage abstractPage;
 	
 	@Step
